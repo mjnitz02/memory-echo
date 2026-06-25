695d9fea88113c6b0747776bcf55a6f6b7b94052
Matt Nitzken <mjnitz02@gmail.com>
2026-06-25 18:35:53 -0400

    Add unit testing

---
+++ MemoryEchoTests/MemoryEchoTests.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryEchoTests.swift
+//  MemoryEchoTests
+//
+//  Created by Matt Nitzken on 6/24/26.
+//
+
+import Testing
+
+struct MemoryEchoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
